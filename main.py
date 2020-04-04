6bf3ec3fd993b89c63e865ec8abcf07e78afb444
teemal <taymal1987@gmail.com>
2020-04-04 08:12:44 -0700

    aggregate rent burden functions

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ api = pygsheets.authorize()
 #gsheet
 wb = api.open('TEST')
 sheet = wb.worksheet_by_title('Sheet2')
-df_route = pd.read_csv('https://openmv.net/file/travel-times.csv')
-sheet.set_dataframe(df_route, (1,1))
 
 fips_codes = {
     "001": "Baker",
@@ -84,61 +82,25 @@ DESCHUTES = '017'
 CROOK = '013'
 JEFFERSON = '031'
 
-# one of many query strings. Below is an example of the string directly below it
 # FINAL_URL = https://api.census.gov/data/2018/acs/acs5?get=B25070_010E&for=county:*&in=state:41
 # this string will get the population of individuals that pay 50% or more of their income
 # in rent for all counties in oregon.
 # It returns a list of lists. The first list containing meta data and the following lists containing the requested info.
 # i.e. one list being ['5690', '41', '047'], meaning 5690 people spend 50% or more of their income on rent in the county 047 (FIPS code for Marion county) in the state 41 (FIPS code for Oregon)
 FINAL_URL = BASE_URL \
-    + GET + GROSS_RENT_PERCENT_INCOME_50_PLUS \
+    + GET + GROSS_RENT_PERCENT_INCOME_50_PLUS + COMMA\
+    + GROSS_RENT_PERCENT_INCOME_30_34 + COMMA\
+    + GROSS_RENT_PERCENT_INCOME_35_39 + COMMA\
+    + GROSS_RENT_PERCENT_INCOME_40_49 + COMMA\
+    + TOTAL_POPULATION_BURDENED\
     + FOR + COUNTY + "*" \
     + IN + STATE + OREGON
 
 r = requests.get(url=FINAL_URL + API_KEY)
 # values is the return value from the census  API
 values = r.json()
-fips_severe_burden = {}
-
-# ==========get population of severe rent burdening by county============================
-for i in range(1, len(values)):
-    fips_severe_burden[values[i][2]] = int(values[i][0])
-
-
-# FINAL_URL = BASE_URL \
-#     + GET + GROSS_RENT_PERCENT_INCOME_30_34 + COMMA + GROSS_RENT_PERCENT_INCOME_35_39 + COMMA + GROSS_RENT_PERCENT_INCOME_40_49\
-#     + FOR + COUNTY + "*" \
-#     + IN + STATE + OREGON
-
-# r = requests.get(url=FINAL_URL + API_KEY)
-# values = r.json()
-# fips_rent_burden = {}
-
-# # ==========get sum population of rent burdening by county==================================
-# for i in range(1, len(values)):
-#     fips_rent_burden[values[i][4]] = int(
-#         values[i][0]) + int(values[i][1]) + int(values[i][2])
-
-# FINAL_URL = BASE_URL \
-#     + GET + TOTAL_POPULATION_BURDENED \
-#     + FOR + COUNTY + "*" \
-#     + IN + STATE + OREGON
-
-# r = requests.get(url=FINAL_URL + API_KEY)
-# values = r.json()
-# fips_population = {}
-
-# # get total population of oregon counties
-# for i in range(1, len(values)):
-#     fips_population[values[i][2]] = int(values[i][0])
-
-
-# # ========HOUSEHOLD  INCOME FOR  ALL BRACKETS IN ALL COUNTIES=================================
-# # household_income is a dict of lists to store all income brackets ($10,000 to $14,999, $15,000 to $19,999,...$200,000+)
+print(values)
 # household_incomes = {}
-# for values in fips_codes.values():
-#     household_incomes[values] = []
-
 # NUM_HOUSEHOLD_INCOME_VARIABLES = 17
 # for i in range(2, NUM_HOUSEHOLD_INCOME_VARIABLES + 1):
 #     # B19001_00 + i + E is a range of income variables in the acs5
@@ -146,7 +108,6 @@ for i in range(1, len(values)):
 #         + GET + ('B19001_00' if i < 10 else 'B19001_0') + str(i) + 'E' \
 #         + FOR + COUNTY + "*" \
 #         + IN + STATE + OREGON
-
 #     r = requests.get(url=FINAL_URL + API_KEY)
 #     values = r.json()
 #     # get number of individuals in ith bracket and match with respective key
@@ -159,10 +120,8 @@ for i in range(1, len(values)):
 #         # household_incom = {Marion: [5690]}
 #         household_incomes[fips_codes[values[i][2]]].append(int(values[i][0]))
 
+
 # trends = {}
-# for values in fips_codes.values():
-#     trends[values] = []
-# # ==========TRENDS IN RENT AND SEVERE BURDENING FROM 2011 to 2018==========================================
 # for i in range(2011,2019):
 #     FINAL_URL =  URL + str(i) + '/' + DATA_SET\
 #     + GET + TOTAL_POPULATION_BURDENED + COMMA + GROSS_RENT_PERCENT_INCOME_50_PLUS + COMMA\
@@ -178,141 +137,6 @@ for i in range(1, len(values)):
 #         trends[fips_codes[values[i][7]]].append(100 * ((int(values[i][2])) + (int(values[i][3])) + (int(values[i][4])))/int(values[i][0]))
 #         trends[fips_codes[values[i][7]]].append(int(values[i][5]))
 
-# # print(household_incomes)
-# # ============make new dicts with key as county name instead of fips=====================
-# population = {}
-# severe_burden_total = {}
-# rent_burden_total = {}
 
-# for key in fips_codes:
-#     if key in fips_population:
-#         population[fips_codes[key]] = fips_population[key]
-#         severe_burden_total[fips_codes[key]] = fips_severe_burden[key]
-#         rent_burden_total[fips_codes[key]] = fips_rent_burden[key]
-
-# # print("pop: \n")
-# # print(population)
-# # print(" ")
-# # print("sev burden: \n")
-# # print(severe_burden_total)
-# # print(" ")
-# # print("burden: \n")
-# # print(rent_burden_total)
-
-# severe_rent_burdened_by_pop = {}
-# rent_burdened_by_pop = {}
-# # ===============new dicts with rent burden/pop as percent=============================
-# for key in population:
-#     if key in rent_burden_total:
-#         # x100 to shift decimal into percent
-#         severe_rent_burdened_by_pop[key] = 100 * \
-#             (severe_burden_total[key] / population[key])
-#         rent_burdened_by_pop[key] = 100 * \
-#             (rent_burden_total[key] / population[key])
-
-# # print("severe rent burdened: ")
-# # print(severe_rent_burdened_by_pop)
-# # print("")
-# # print("rent burdened:")
-# # print(rent_burdened_by_pop)
-
-
-# # ===========================XLS STUFF=====================================================
-# workbook = xlsxwriter.Workbook('data.xlsx')
-# worksheet = workbook.add_worksheet('rent_burdening')
-# row = 0
-# col = 0
-# col_names = ['county', 'sample population', 'percent rent burdened',
-#              'percent severly rent burdened']
-# for i in col_names:
-#     worksheet.write(row, col, i)
-#     col += 1
-# row = 1
-# col = 0
-# # there's probably a really slick way to  do this with a lambda
-# # but I'm tired and this works
-# for i in population:
-#     worksheet.write(row, col, i)
-#     col += 1
-#     worksheet.write(row, col, population[i])
-#     col += 1
-#     worksheet.write(row, col, rent_burdened_by_pop[i])
-#     col += 1
-#     worksheet.write(row, col, severe_rent_burdened_by_pop[i])
-#     row += 1
-#     col = 0
-
-
-# income_brackets = [
-# "Less than $10,000",
-# "$10,000 to $14,999",
-# "$15,000 to $19,999",
-# "$20,000 to $24,999",
-# "$25,000 to $29,999",
-# "$30,000 to $34,999",
-# "$35,000 to $39,999",
-# "$40,000 to $44,999",
-# "$45,000 to $49,999",
-# "$50,000 to $59,999",
-# "$60,000 to $74,999",
-# "$75,000 to $99,999",
-# "$100,000 to $124,999",
-# "$125,000 to $149,999",
-# "$150,000 to $199,999",
-# "$200,000 or more"
-# ]
-
-# # new sheet
-# worksheet = workbook.add_worksheet('household incomes')
-# row = 0
-# col = 0
-# # write header to sheet
-# worksheet.write(row, col, 'county')
-# for i in income_brackets:
-#     col += 1
-#     worksheet.write(row, col, i)
-
-# # write incomes per county by brackets to sheet
-# row = 1
-# col = 0
-# for key in household_incomes:
-#     worksheet.write(row,col,key)
-#     for i in household_incomes[key]:
-#         col += 1
-#         worksheet.write(row,col,i)
-#     col = 0
-#     row += 1
-
-# # new sheet
-# worksheet = workbook.add_worksheet('historic data')
-# row = 0
-# col = 0
-# columns = ['county', 'severe rent burdening', 'rent burdening', 'median gross income']
-# # write header to sheet
-# for i in columns:
-#     worksheet.write(row, col, i)
-#     col += 1
-
-# # write counties and years (2011 - 2018) to xls
-# row = 1
-# col = 0
-# start_year = 2011
-# end_year = 2018
-# for key in trends:
-#     for i in range(start_year, end_year + 1):
-#         worksheet.write(row,col, key + str(i))
-#         row += 1
-
-# row = 1
-# col = 1
-# # this goes through the sheet and adds count+year then three data columns (severe, burdened, median gross income)
-# # then it drops a row and starts the process over starting at column 0
-# for key in trends:
-#     for i in trends[key]:
-#         if (col > 3):
-#             col = 1
-#             row += 1
-#         worksheet.write(row, col, i)
-#         col += 1
-# workbook.close()
-# print("done")
+# df_route = pd.read_csv('https://openmv.net/file/travel-times.csv')
+# sheet.set_dataframe(df_route, (1,1))
